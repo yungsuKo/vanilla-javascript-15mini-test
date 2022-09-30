4a47db5c306cb84522d6f1e13bc956fa979b8232
Tom Ko <yungsu2391@gmail.com>
2022-09-30 10:21:26 +0900

    toggle, how to select currentTarget element

--- question/app.js
+++ question/app.js
@@ -1,2 +1,14 @@
 //using selectors inside the element
+
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+const questionText = document.querySelector(".question-text");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+    console.log("click");
+  });
+});
